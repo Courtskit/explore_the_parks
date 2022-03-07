35448e8f0e4480feed283a8f41da111616de139a
Courtney <courtskit@gmail.com>
2022-03-07 09:49:30 -0800

    set up files

--- parks/urls.py
+++ parks/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.index),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
 ]
--- parks/views.py
+++ parks/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+import requests
+from requests_oauthlib import OAuth1
+import os
+from dotenv import load_dotenv
 
-# Create your views here.
+def index(request):
+    home_page_data = { 
+        "park": 'info'
+    }
+    return render(request, "pages/index.html", home_page_data)
+
+def about(request):
+    auth = OAuth1(os.environ['key'])
+    endpoint = f"https://developer.nps.gov/api/v1/alerts"
+    response = requests.get(endpoint, auth=auth)
+    responseJSON = response.json()
+    print(responseJSON)
+    np_data = { 
+        "key": 'value'
+    }
+    return render(request, "pages/about.html", np_data)
+
+def contact(request):
+    return render(request, "pages/contact.html")
